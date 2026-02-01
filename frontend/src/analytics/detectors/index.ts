29a909d07a4ccf754441c2d11b77339499307760
ItzTheLegend <ianfigueroa12345@gmail.com>
2026-01-31 22:17:27 -0400

    added LargeTradeDetector for market pulse analysis

--- frontend/src/analytics/detectors/index.ts
+++ frontend/src/analytics/detectors/index.ts
@@ -1,2 +1,4 @@
 export { IcebergDetector, type IcebergSignal } from './IcebergDetector';
 export { LiquidityZoneDetector } from './LiquidityZoneDetector';
+export { LargeTradeDetector } from './LargeTradeDetector';
+export type { LargeTradeSignal, LargeTradeSignalType, LargeTradeConfig } from './LargeTradeDetector';
